b56d4c44f7b0a0d8361bb3b027341578eb2bfbb4
Asymmetrical Shari <ashari@mi-w6.21-school.ru>
2019-04-25 18:09:20 +0300

    Drawing a line with norm coord

---
+++ includes/fdf.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ashari <ashari@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/25 17:25:28 by ashari            #+#    #+#             */
+/*   Updated: 2019/04/25 17:45:51 by ashari           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <mlx.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef	struct		fdf
+{
+	int				x1;
+	int				x2;
+	int				y1;
+	int				y2;
+	int				color;
+	int				win_size;
+	void			*mlx_ptr;
+	void			*win_ptr;
+	struct fdf		*next;
+}					t_fdf;
+
+int				ft_brezen(t_fdf *map);
+
+#endif
